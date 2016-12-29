aa272542c156cdce25ee8b2e1ce8fbcbdb280332
mtharmen <m.tharmendran@gmail.com>
2016-12-29 13:01:38 -0500

    Update setup.js

--- config/setup.js
+++ config/setup.js
@@ -12,7 +12,7 @@ module.exports = function(app, base) {
 
 	// Mongoose setup
 	mongoose.Promise = global.Promise;
-	var mongodbUrl = process.env.MONGODB_URL || 'mongodb://locahlhost:8080'
+	var mongodbUrl = process.env.MONGODB_URL || 'mongodb://localhost:8080'
 	
 	mongoose.connect(mongodbUrl + '/mtharmen-voting-app');
 	var db = mongoose.connection;
